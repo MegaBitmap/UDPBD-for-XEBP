dbc43de30ed04b10e8c9638bc9c11d34c4bd364c
MegaBitmap <93689649+MegaBitmap@users.noreply.github.com>
2024-09-19 12:14:19 -0700

    Revert "for loop fix"
    This reverts commit eb439fd13c53e44999f592c9a70779ea106f4748.

--- XEBPLUS/APPS/neutrinoUDPBD/neutrinoLauncherUDPBD.lua
+++ XEBPLUS/APPS/neutrinoUDPBD/neutrinoLauncherUDPBD.lua
@@ -1758,7 +1758,7 @@ for NEUTRINO_i = 1, 3 do
 	oldpad = pad;
 	Screen.flip()
 end
-for NEUTRINO_i = -90, -111, -1 do
+for NEUTRINO_i = -90, -111 do
 	if themeInUse[NEUTRINo_i] > 0 then
 		Graphics.freeImage(themeInUse[NEUTRINO_i])
 	end
