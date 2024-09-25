01f2284e472e1a0bbe730260450c4f17c5cc0826
MegaBitmap <93689649+MegaBitmap@users.noreply.github.com>
2024-09-25 10:44:10 -0700

    Add launchELF shortcut

---
+++ XEBPLUS/PLG/PLG_LaunchELF.lua
@@ -0,0 +1,10 @@
+PluginData = {};
+PluginData.Type = "SingleELF";
+PluginData.Category = 6;
+PluginData.Name = "LaunchELF";
+PluginData.Description = xebLang[31];
+PluginData.Icon = 113;
+PluginData.Safe = true;
+PluginData.ValueA = "mc0:/BOOT/BOOT.ELF";
+PluginData.ValueB = "mc1:/BOOT/BOOT.ELF";
+PluginData.ValueC = "mass:/BOOT/BOOT.ELF";
