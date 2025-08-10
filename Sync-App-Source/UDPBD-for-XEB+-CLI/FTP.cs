2ce9a2fb36366c7ac6b766c9fe7d7b8a28953db9
MegaBitmap <93689649+MegaBitmap@users.noreply.github.com>
2025-08-10 14:55:39 -0700

    Bugfixes for FTP methods
    for compatibility with the ps2 ftp driver ps2ftpd, reconnect every time FtpDataStream is used.
    more automatic retries.
    added more console output for exceptions.

--- Sync-App-Source/UDPBD-for-XEB+-CLI/FTP.cs
+++ Sync-App-Source/UDPBD-for-XEB+-CLI/FTP.cs
@@ -8,28 +8,15 @@ namespace UDPBD_for_XEB__CLI
     {
         public static bool TestConnection(FtpClient client, IPAddress ps2ip)
         {
-            try
-            {
-                Ping pingSender = new();
-                PingReply reply = pingSender.Send(ps2ip, 6000);
-                if (!(reply.Status == IPStatus.Success))
-                {
-                    Console.WriteLine($"\nCONNECTION FAILED\n\nFailed to receive a ping reply:\nPlease verify that your network settings are configured properly and all cables are connected.\nTry adjusting the IP address settings in launchELF.\n{reply.Status}");
-                    return false;
-                }
-            }
-            catch (PingException ex)
-            {
-                Console.WriteLine($"\nCONNECTION FAILED\n\nThe network location cannot be reached:\nPlease verify that your network settings are configured properly and all cables are connected.\nTry manually assigning an IPv4 address and subnet mask to this PC.\n{ex.Message}");
-                return false;
-            }
+            if (!CheckPing(ps2ip)) {  return false; }
             try
             {
                 client.Connect();
-                Thread.Sleep(200);
-                client.GetListing();
-                Thread.Sleep(200);
+                client.GetListing(); // for compatibility with ps2ftpd, reconnect every time FtpDataStream is used
+                client.Disconnect();
+                client.Connect();
                 Console.WriteLine("Connected to the PS2's FTP server Successfully!");
+                
                 return true;
             }
             catch (Exception ex)
@@ -40,75 +27,114 @@ namespace UDPBD_for_XEB__CLI
             }
         }
 
-        public static void CreateDirectory(FtpClient client, string directoryPath)
+        public static bool CheckPing(IPAddress ps2ip)
         {
-            try
+            for (int i = 0; i < 5; i++)
             {
-                client.CreateDirectory(directoryPath);
-                Thread.Sleep(200);
-                if (!DirectoryExists(client, directoryPath))
+                try
                 {
-                    Console.WriteLine($"Failed to create the directory {directoryPath} on the PS2 via FTP.\nNo exceptions raised.");
-                    client.Disconnect();
-                    Program.PauseExit(11);
+                    Ping pingSender = new();
+                    PingReply reply = pingSender.Send(ps2ip, 6000);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\nCONNECTION FAILED\n\nFailed to receive a ping reply:\nPlease verify that your network settings are configured properly and all cables are connected.\nTry adjusting the IP address settings in launchELF.\n{reply.Status}");
+                    }
+                }
+                catch (PingException ex)
+                {
+                    Console.WriteLine($"\nCONNECTION FAILED\n\nThe network location cannot be reached:\nPlease verify that your network settings are configured properly and all cables are connected.\nTry manually assigning an IPv4 address and subnet mask to this PC.\n{ex.Message}");
                 }
             }
-            catch (Exception ex)
+            return false;
+        }
+
+        public static void CreateDirectory(FtpClient client, string directoryPath)
+        {
+            for (int i = 0;i < 5; i++)
             {
-                Console.WriteLine($"Failed to create the directory {directoryPath} on the PS2 via FTP.\n{ex.Message}");
-                client.Disconnect();
-                Program.PauseExit(12);
+                try
+                {
+                    client.CreateDirectory(directoryPath);
+                    if (!DirectoryExists(client, directoryPath))
+                    {
+                        Console.WriteLine($"Failed to create the directory {directoryPath} on the PS2 via FTP.\nNo exceptions raised.");
+                        client.Disconnect();
+                        client.Connect();
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to create the directory {directoryPath} on the PS2 via FTP.\n{ex.Message}");
+                    client.Disconnect();
+                    client.Connect();
+                }
             }
+            client.Disconnect();
+            Program.PauseExit(11);
         }
 
         public static bool DirectoryExists(FtpClient client, string directoryPath)
         {
             try
             {
+                client.GetListing(directoryPath); // for compatibility with ps2ftpd, reconnect every time FtpDataStream is used
+                client.Disconnect();
+                client.Connect();
                 client.GetListing(directoryPath);
-                Thread.Sleep(200);
-                client.GetListing(directoryPath); // Try twice because the launchELF ftp server is strange
-                Thread.Sleep(200);
+                client.Disconnect();
+                client.Connect();
                 return true;
             }
             catch
             {
-                Thread.Sleep(200);
+                client.Disconnect();
+                client.Connect();
                 return false;
             }
         }
 
         public static void UploadFile(FtpClient client, string filePath, string ftpPath, string targetFile)
         {
-            try
+            for (int i = 0; i < 5; i++)
             {
-                client.UploadFile(Path.GetFullPath(filePath), ftpPath + targetFile);
-                Thread.Sleep(1000);
-                if (!FileExists(client, ftpPath, targetFile))
+                try
                 {
-                    Console.WriteLine($"Failed to upload file {filePath} to the PS2 via FTP.\nNo exceptions raised.");
+                    if (FileExists(client, ftpPath, targetFile))
+                    {
+                        client.DeleteFile(ftpPath + targetFile);
+                    }
+                    client.UploadFile(Path.GetFullPath(filePath), ftpPath + targetFile, FtpRemoteExists.NoCheck); // for compatibility with ps2ftpd, reconnect every time FtpDataStream is used
+                    // FtpRemoteExists.NoCheck is needed otherwise internal fluentFTP methods will use FtpDataStream more than once
                     client.Disconnect();
-                    Program.PauseExit(13);
-                }
-                FileInfo fileInfo = new(filePath);
-                long ftpSize = GetSize(client, ftpPath, targetFile);
-                for (int i = 0; i < 3; i++) // Try 3 times to match the file size before throwing an error
-                {
+                    client.Connect();
+                    if (!FileExists(client, ftpPath, targetFile))
+                    {
+                        Console.WriteLine($"Failed to upload file {filePath} to the PS2 via FTP.\nNo exceptions raised.\nRetrying . . .");
+                    }
+                    FileInfo fileInfo = new(filePath);
+                    long ftpSize = GetSize(client, ftpPath, targetFile);
                     if (ftpSize == fileInfo.Length) { return; }
-                    ftpSize = GetSize(client, ftpPath, targetFile);
+                    Console.WriteLine($"Target file size: {fileInfo.Length}");
+                    Console.WriteLine($"File size reported by launchELF: {ftpSize}");
+                    Console.WriteLine($"Failed to upload file {filePath} to the PS2 via FTP.\nWrong file size.\nRetrying . . .");
+
                 }
-                Console.WriteLine($"Target file size: {fileInfo.Length}");
-                Console.WriteLine($"File size reported by launchELF: {ftpSize}");
-                Console.WriteLine($"Failed to upload file {filePath} to the PS2 via FTP.\nWrong file size.");
-                client.Disconnect();
-                Program.PauseExit(13);
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Failed to upload file {filePath} to the PS2 via FTP.\n{ex.Message}\n{ex.InnerException}\nRetrying . . .");
+                }
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Failed to upload file {filePath} to the PS2 via FTP.\n{ex.Message}\n{ex.InnerException}");
-                client.Disconnect();
-                Program.PauseExit(14);
-            }
+            Console.WriteLine("FTP.UploadFile Failed too many times.");
+            client.Disconnect();
+            Program.PauseExit(13);
         }
 
         public static string GetDir(FtpClient client, string ftpPath)
@@ -116,49 +142,50 @@ namespace UDPBD_for_XEB__CLI
             try
             {
                 string returnList = "";
-                var ftpList = client.GetListing(ftpPath);
-                Thread.Sleep(200);
-                var ftpList2 = client.GetListing(ftpPath); // Try twice because the launchELF ftp server is strange
-                Thread.Sleep(200);
-                foreach (var item in ftpList)
+                for (int i = 0; i < 2; i++)
                 {
-                    returnList += $" {item.Name} ";
-                }
-                foreach (var item in ftpList2)
-                {
-                    if (!returnList.Contains(item.ToString()))
+                    var ftpList = client.GetListing(ftpPath); // for compatibility with ps2ftpd, reconnect every time FtpDataStream is used
+                    client.Disconnect();
+                    client.Connect();
+                    foreach (var item in ftpList)
                     {
-                        returnList += $" {item.Name} ";
+                        if (!returnList.Contains(item.ToString()))
+                        {
+                            returnList += $" {item.Name} ";
+                        }
                     }
                 }
                 return returnList;
             }
-            catch
+            catch (Exception ex)
             {
-                Thread.Sleep(200);
+                Console.WriteLine($"Failed to list {ftpPath} via FTP.\n{ex.Message}\n{ex.InnerException}");
+                client.Disconnect();
+                client.Connect();
                 return "";
             }
         }
 
         public static long GetSize(FtpClient client, string ftpPath, string file)
         {
-            var ftpList = client.GetListing(ftpPath);
-            Thread.Sleep(200);
-            var ftpList2 = client.GetListing(ftpPath);
-            Thread.Sleep(200);
-            foreach (var item in ftpList)
+            try
             {
-                if (item.Name == file)
+                var ftpList = client.GetListing(ftpPath); // for compatibility with ps2ftpd, reconnect every time FtpDataStream is used
+                client.Disconnect();
+                client.Connect();
+                foreach (var item in ftpList)
                 {
-                    return item.Size;
+                    if (item.Name == file)
+                    {
+                        return item.Size;
+                    }
                 }
             }
-            foreach (var item in ftpList2)
+            catch (Exception ex)
             {
-                if (item.Name == file)
-                {
-                    return item.Size;
-                }
+                Console.WriteLine($"Failed to list {ftpPath} via FTP.\n{ex.Message}\n{ex.InnerException}");
+                client.Disconnect();
+                client.Connect();
             }
             return 0;
         }
@@ -167,18 +194,10 @@ namespace UDPBD_for_XEB__CLI
         {
             try
             {
-                var files = client.GetListing(ftpPath);
-                Thread.Sleep(200);
+                var files = client.GetListing(ftpPath); // for compatibility with ps2ftpd, reconnect every time FtpDataStream is used
+                client.Disconnect();
+                client.Connect();
                 foreach (var file in files)
-                {
-                    if (file.Name.Contains(ftpFile))
-                    {
-                        return true;
-                    }
-                }
-                var files2 = client.GetListing(ftpPath); // Try twice because the launchELF ftp server is strange
-                Thread.Sleep(200);
-                foreach (var file in files2)
                 {
                     if (file.Name.Contains(ftpFile))
                     {
@@ -187,9 +206,11 @@ namespace UDPBD_for_XEB__CLI
                 }
                 return false;
             }
-            catch
+            catch (Exception ex)
             {
-                Thread.Sleep(200);
+                Console.WriteLine($"Failed to list {ftpPath} via FTP.\n{ex.Message}\n{ex.InnerException}");
+                client.Disconnect();
+                client.Connect();
                 return false;
             }
         }
